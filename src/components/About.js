9e0407e366d6d35ea85bbff3f4405a13af468167
pascal-maker <pascalmusabyimana681@gmail.com>
2023-03-24 17:42:24 +0100

    fixes

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
             <div className='left'>
                 <h1>About</h1>
                 <p> VEX-OH comes from the word to vex which means no stress in Pidgin English. That is actually our goal people should not stress about which pictures they need to upload in order to come across as perfect. Instead they can just open the camera and show the world who they really are. </p>
-                <Link to='/https://www.linkedin.com/company/lovia-app/?viewAsMember=true'><button className='btn'>Contact</button></Link>
+                <Link to='https://www.linkedin.com/company/lovia-app/?viewAsMember=true'><button className='btn'>Contact</button></Link>
             </div>
             <div className='right'>
                 <div className='img-container'>
--- src/components/Download.js
+++ src/components/Download.js
@@ -1,41 +1,50 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './DownloadStyles.css'
-
+import React from "react";
+import "./DownloadStyles.css";
 
 const Download = () => {
-    return (
-        <div className='Pricing'>
-            <div className='card-container'>
-                <div className='card'>
-                    <h3>- IOS -</h3>
-                    <span className='bar'></span>
-                    <p className='Ios'>Appstore</p>
-                    <p>- Dual-Camera -</p>
-                    <p>- 1 Picture -</p>
-                    <p>- 24 hours available -</p>
-                    <p>- Have fun and Explore ! -</p>
-                   <Link to='/https://apps.apple.com/be/app/lovia/id1574149291?l=nl' className='btn'>Download</Link>
-                </div>
+  return (
+    <div className="Pricing">
+      <div className="card-container">
+        <div className="card">
+          <h3>- IOS -</h3>
+          <span className="bar"></span>
+          <p className="Ios">Appstore</p>
+          <p>- Dual-Camera -</p>
+          <p>- 1 Picture -</p>
+          <p>- 24 hours available -</p>
+          <p>- Have fun and Explore ! -</p>
 
-                <div className='card'>
-                    <h3>- Android -</h3>
-                    <span className='bar'></span>
-                    <p className='Google'> Playstore</p>
-                    <p>- Dual-Camera -</p>
-                    <p>- 1 Picture -</p>
-                    <p>- 24 hours Available-</p>
-                    <p>- Have fun and Explore ! -</p>
-    
-
-                    <Link to='/https://play.google.com/store/apps/details?id=com.lovialove&pli=1' className='btn'>Download</Link>
-                    
- 
-
-                </div>
-            </div>
+          <a
+            href="https://apps.apple.com/be/app/lovia/id1574149291?l=nl"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="btn"
+          >
+            Download
+          </a>
         </div>
-    )
-}
 
-export default Download
+        <div className="card">
+          <h3>- Android -</h3>
+          <span className="bar"></span>
+          <p className="Google"> Playstore</p>
+          <p>- Dual-Camera -</p>
+          <p>- 1 Picture -</p>
+          <p>- 24 hours Available-</p>
+          <p>- Have fun and Explore ! -</p>
+
+          <a
+            href="https://play.google.com/store/apps/details?id=com.lovialove&pli=1"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="btn"
+          >
+            Download
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
